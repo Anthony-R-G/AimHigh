6961a211f774acec86165c708bdcdd88f66e1310
AGonzalez5195 <AnthonyGonzalez@pursuit.org>
2020-06-22 18:53:19 -0400

    add podfile

---
+++ AimHigh/AimHigh/Views/CustomShapeView.swift
@@ -0,0 +1,21 @@
+//
+//  CustomShapeView.swift
+//  AimHigh
+//
+//  Created by Anthony Gonzalez on 6/16/20.
+//  Copyright © 2020 Chisme y Cafecito. All rights reserved.
+//
+
+import UIKit
+
+class CustomShapeView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
